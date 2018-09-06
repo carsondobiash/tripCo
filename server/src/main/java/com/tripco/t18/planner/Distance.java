44e33c89cc56104c2e50ee5db58423e5af9a42e0
bjjungen135 <bjjungen@gmail.com>
2018-09-06 16:04:21 -0600

    Change to Distance class, was doing the to radians conversion

--- server/src/main/java/com/tripco/t18/planner/Distance.java
+++ server/src/main/java/com/tripco/t18/planner/Distance.java
@@ -11,19 +11,25 @@ public class Distance {
     public int distance;
 
     public int  calculatedistance() {
+        //Seting up latitude and longitude in radians
+        double latitude_origin = Math.toRadians(this.origin.latitude);
+        double longitdue_origin = Math.toRadians(this.origin.longitude);
+        double latitude_destination = Math.toRadians(this.destination.latitude);
+        double longitude_destination = Math.toRadians(this.destination.longitude);
+
         //Variables created for calculations
-        double difflat = Math.abs(this.origin.latitude - this.destination.latitude);
-        double difflon = Math.abs(this.origin.longitude - this.destination.longitude);
+        double difflat = Math.abs(latitude_origin - latitude_destination);
+        double difflon = Math.abs(longitdue_origin - longitude_destination);
         int radius_miles = 3959;
         int radius_nautical_miles = 3440;
         int radius_kilometers = 6371;
 
         //Creates top half of the vincenty formula
-        double tophav = Math.sqrt(Math.pow(2, (Math.cos(this.destination.latitude) * Math.sin(difflon))) +
-        Math.pow(2, (Math.cos(this.origin.latitude) * Math.sin(this.destination.latitude) - (Math.sin(this.origin.latitude) * Math.cos(this.destination.latitude) * Math.cos(difflon)))));
+        double tophav = Math.sqrt(Math.pow(2, (Math.cos(latitude_destination) * Math.sin(difflon))) +
+        Math.pow(2, (Math.cos(latitude_origin) * Math.sin(latitude_destination) - (Math.sin(latitude_origin) * Math.cos(latitude_destination) * Math.cos(difflon)))));
 
         //Creates bottom half of the vincenty formula
-        double bothav = (Math.sin(this.origin.latitude) * Math.sin(this.destination.latitude)) + (Math.cos(this.origin.latitude) * Math.cos(this.destination.latitude) * Math.cos(difflon));
+        double bothav = (Math.sin(latitude_origin) * Math.sin(latitude_destination)) + (Math.cos(latitude_origin) * Math.cos(latitude_destination) * Math.cos(difflon));
 
         //Gets the vincenty number to be multiplied by the radius of the earth
         double vincenty = Math.atan2(tophav, bothav);
