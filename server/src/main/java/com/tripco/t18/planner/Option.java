7282a03bd5ac34e9f06004b39a83d67aea891418
Daniel <veez@rams.colostate.edu>
2018-09-18 13:31:08 -0600

    update trip to support custom units

--- server/src/main/java/com/tripco/t18/planner/Option.java
+++ server/src/main/java/com/tripco/t18/planner/Option.java
@@ -1,5 +1,8 @@
 package com.tripco.t18.planner;
 
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Describes the options to apply when planning a trip in TFFI format.
  * At this point we are only using the values provided.
@@ -8,5 +11,8 @@ public class Option {
 
   public String distance;
   public String optimization;
+  public List<String> units = Arrays.asList("miles","nautical miles","kilometers", "user defined" );
+  public String unitName;
+  public int unitRadius;
 
 }
