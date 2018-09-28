c15aa77c34001d37c7fe9492fbeaaf2abdd29db3
carsondobiash <carsond77@comcast.net>
2018-09-27 19:38:08 -0600

    JSON files with trip data can now be uploaded.

--- client/src/components/Application/Trip.js
+++ client/src/components/Application/Trip.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react'
-import { Card, CardHeader, CardBody } from 'reactstrap'
-import LoadTrip from "./LoadTrip";
+import {Card, CardHeader, CardBody, Button} from 'reactstrap'
 
 /* Options allows the user to change the parameters for planning
  * and rendering the trip map and itinerary.
@@ -10,14 +9,43 @@ import LoadTrip from "./LoadTrip";
 class Trip extends Component{
     constructor(props) {
         super(props);
+        this.upload = this.upload.bind(this);
+        this.fileOnChange = this.fileOnChange.bind(this);
+
+        this.state = {file: null}
+
     }
+
+    fileOnChange(event){
+        let f = event.target.files[0];
+        this.setState({'file': f})
+    }
+
+    upload(){
+        let file = this.state.file;
+        if(file){
+            let reader = new FileReader();
+            reader.onload =  function(event){
+                //console.log(event.target.result);
+                let parsed = JSON.parse(event.target.result);
+                this.props.updateBasedOnResponse(parsed);
+            }.bind(this);
+            reader.readAsText(file);
+        }
+    }
+
+
+
 
     render() {
         return(
             <Card>
                 <CardBody>
                     <p>Trip</p>
-                    <LoadTrip/>
+                    <form>
+                        <input type="file" title="input" onChange={this.fileOnChange}/>
+                        <button type="submit" onClick={this.upload}>Load</button>
+                    </form>
                 </CardBody>
             </Card>
         )
