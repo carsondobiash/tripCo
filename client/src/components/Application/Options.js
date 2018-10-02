3f30a51b4bce5d7a07a902ef7312c1b745109a9a
Daniel <veez@rams.colostate.edu>
2018-10-02 14:50:32 -0600

    bugfixes

--- client/src/components/Application/Options.js
+++ client/src/components/Application/Options.js
@@ -33,7 +33,7 @@ class Options extends Component{
             <Form inline>
                 <br />
                 <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
-                    <input type="text" placeholder="Enter unit name" value={this.props.options.unitName} onChange={(event)=> this.props.updateOptions('unitName', event.target.value) }/>
+                    <input type="text" placeholder="Enter unit name" value={null} onChange={(event)=> this.props.updateOptions('unitName', event.target.value) }/>
                     <input type="number" name="password" placeholder="Enter unit radius" value={null} onChange={(event)=> this.props.updateOptions('unitRadius', event.target.value) }/>
                 </FormGroup>
             </Form>
