e4931108a99acea5d1b554bac6731afbc5f05c84
Jacob Augustine <42559658+jtaugust@users.noreply.github.com>
2018-10-18 21:18:31 -0600

    Update Config.java

--- server/src/main/java/com/tripco/t18/server/Config.java
+++ server/src/main/java/com/tripco/t18/server/Config.java
@@ -2,15 +2,17 @@ package com.tripco.t18.server;
 
 import com.google.gson.Gson;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 public class Config {
 
   private short version = 3;
   private String type = "config";
-
-  private List<String> units = Arrays.asList("miles","nautical miles","kilometers", "user defined" );
+  private List<String> units = Arrays.asList("miles","nautical miles","kilometers", "user defined");
+  private List<String> optimization = Arrays.asList("none", "short", "shorter");
 
   static String getConfig() {
     Config conf = new Config();
