bbd789e617e8acd36c437ee531ec512e2d9f4a2d
Daniel <veez@rams.colostate.edu>
2018-10-29 15:19:02 -0600

    update for tffi specifically for optimizations

--- server/src/main/java/com/tripco/t18/server/Config.java
+++ server/src/main/java/com/tripco/t18/server/Config.java
@@ -2,15 +2,31 @@ package com.tripco.t18.server;
 
 import com.google.gson.Gson;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class Config {
 
   private short version = 3;
   private String type = "config";
   private List<String> units = Arrays.asList("miles","nautical miles","kilometers", "user defined");
-  private List<String> optimization = Arrays.asList("none", "short", "shorter");
+  private List optimizations;
+
+  public Config(){
+      Map<String, String> none = new HashMap<>();
+      none.put("label", "none");
+      none.put("description", "The trip is not optimized.");
+      Map<String, String> shorty = new HashMap<>();
+      shorty.put("label", "short");
+      shorty.put("description", "Nearest neighbor.");
+      Map<String, String> shorter = new HashMap<>();
+      shorter.put("label", "shorter");
+      shorter.put("description", "2-opt.");
+
+      optimizations.add(none);
+      optimizations.add(shorty);
+      optimizations.add(shorter);
+
+  }
 
   static String getConfig() {
     Config conf = new Config();
