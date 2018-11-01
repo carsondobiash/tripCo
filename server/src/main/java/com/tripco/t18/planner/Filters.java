8aeb9d6d7c11fcaf20ae0eb390b2c67d350baec4
bjjungen135 <bjjungen@gmail.com>
2018-10-31 22:01:32 -0600

    Updating Config and implementing the found in database. Then adding a test to it. Also fixed me adding a default optimization to option.

---
+++ server/src/main/java/com/tripco/t18/planner/Filters.java
@@ -0,0 +1,8 @@
+package com.tripco.t18.planner;
+
+import java.util.*;
+
+public class Filters {
+    public String name;
+    public ArrayList<Map<String, String>> filters;
+}
--- server/src/test/java/com/tripco/t18/planner/TestDatabase.java
+++ server/src/test/java/com/tripco/t18/planner/TestDatabase.java
@@ -36,9 +36,11 @@ public class TestDatabase {
     @Test
     public void testSearchString(){
         database.databaseSearch();
+        System.out.println(database.found);
         assertEquals(true, database.places.get(0).id.equals(testPlaces.id));
         assertEquals(true, database.places.get(0).name.equals(testPlaces.name));
         assertEquals(true, database.places.get(0).latitude == testPlaces.latitude);
         assertEquals(true, database.places.get(0).longitude == testPlaces.longitude);
+        assertEquals(true, database.found == 30);
     }
 }
